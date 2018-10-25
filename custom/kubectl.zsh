c8fbe2a8b329a80ba089e99a42c74f57c42e5ad7
Freddy Esteban Perez <fredstban@icloud.com>
2018-10-24 22:36:33 -0500

    updated juanghurtado theme and added kubectl context

---
+++ custom/kubectl.zsh
@@ -0,0 +1,11 @@
+function _get_k8s_context() {
+    local context
+
+    kubeconfig="$HOME/.kube/config"
+    if [[ -n "$KUBECONFIG" ]]; then
+        kubeconfig="$KUBECONFIG"
+    fi
+
+    context="$(kubectl config current-context 2>/dev/null)"
+    echo "($context)"
+}
